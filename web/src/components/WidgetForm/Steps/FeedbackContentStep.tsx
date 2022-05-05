4609ada8c019ec85d4df73a6399a2b01d395622d
Marcelo Augusto Martinho Gomes <mgomes.dev@gmail.com>
2022-05-04 23:07:51 -0300

    created FeedbackSuccessStep component and methods

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -62,7 +62,7 @@ export function FeedbackContentStep(props: FeedbackContentStepProps) {
                     <button
                         type="submit"
                         disabled={!comment.length}
-                        className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
+                        className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 disabled:opacity-50 disabled:hover:bg-brand-500 transition-colors"
                     >
                         Enviar feedback
                     </button>
