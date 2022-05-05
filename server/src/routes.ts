2e840c193f3a9245f1b582bf9a25fdf9fd7936bc
Marcelo Augusto Martinho Gomes <mgomes.dev@gmail.com>
2022-05-05 03:01:04 -0300

    back/moved feedback-create method and prisma client to own files

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,7 @@
+import { PrismaFeedbacksRepository } from './repositories/prisma/prisma-feedbacks-repository';
 import express from "express";
-import { prisma } from './prisma';
 import nodemailer from "nodemailer";
+import { SubmitFeedbackUseCase } from "./use-cases/submit-feedback-use-case";
 
 export const routes = express.Router();
 
@@ -16,13 +17,10 @@ const transport = nodemailer.createTransport({
 routes.post("/feedbacks", async (req, res) => {
     const { type, comment, screenshot } = req.body;
 
-    const feedback = await prisma.feedback.create({
-        data: {
-            type,
-            comment,
-            screenshot
-        }
-    });
+    const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+    const submitFeedbackUseCase = new SubmitFeedbackUseCase(prismaFeedbacksRepository);
+
+    await submitFeedbackUseCase.execute({ type, comment, screenshot });
 
     await transport.sendMail({
         from: "Equipe Feedget <oi@feedget.com>",
@@ -36,5 +34,5 @@ routes.post("/feedbacks", async (req, res) => {
         ].join("\n")
     })
 
-    return res.status(201).json({ data: feedback })
+    return res.status(201).send();
 });
