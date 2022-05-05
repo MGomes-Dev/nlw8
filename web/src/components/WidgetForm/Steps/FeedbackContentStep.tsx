6bf2b1ca643fd1e008f3f30554595922abf9bf66
Marcelo Augusto Martinho Gomes <mgomes.dev@gmail.com>
2022-05-04 22:20:20 -0300

    created structure to send feedback (but not sending yet)

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,5 +1,5 @@
-import { ArrowLeft, Camera } from "phosphor-react";
-import { useState } from "react";
+import { ArrowLeft } from "phosphor-react";
+import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
 import { ScreenshotButton } from "../ScreenshotButton";
@@ -11,8 +11,17 @@ interface FeedbackContentStepProps {
 
 export function FeedbackContentStep(props: FeedbackContentStepProps) {
     const [screenshot, setScreenshot] = useState<string | null>(null);
+    const [comment, setComment] = useState("");
 
     const feedbacktypeInfo = feedbackTypes[props.feedbackType];
+
+    function handleSubmitFeedback(event: FormEvent) {
+        event.preventDefault();
+        console.log({
+            screenshot,
+            comment
+        })
+    };
 
     return (
         <>
@@ -22,7 +31,7 @@ export function FeedbackContentStep(props: FeedbackContentStepProps) {
                     className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
                     onClick={props.onFeedbackRestarRequested}
                 >
-                    <ArrowLeft weight="bold" className="w-4 h-4"/>
+                    <ArrowLeft weight="bold" className="w-4 h-4" />
                 </button>
 
                 <span className="text-xl leading-6 flex items-center gap-2">
@@ -33,8 +42,9 @@ export function FeedbackContentStep(props: FeedbackContentStepProps) {
                 <CloseButton />
             </header>
 
-            <form className="my-4 w-full">
+            <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
                 <textarea
+                    onChange={event => setComment(event.target.value)}
                     className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
                     placeholder="Conte com detalhes o que está acontecendo..."
                 />
